253d7c5c6d1edf1bd85ff2db28e67298f95acb46
zhangGuojin <zhanggjjim@163.com>
2017-03-30 22:22:44 +0800

    去掉for循环中的await

--- src/spider_ctrl/handler.js
+++ src/spider_ctrl/handler.js
@@ -14,23 +14,31 @@ export default new class {
       const items = await promise;
       const { data } = items;
 
-      for (let item of data) {
-        item = util.filter(item, ['title', 'description', 'url', 'display_time', 'thumb', 'id']);
-        const articleText = await SpiderArticle.scratch(item.url);
-        item.text = articleText;
-        const display_time = util.formatTime(item.display_time);
-        item.display_time = display_time && new Date(display_time).toISOString();
-        const artExist = await spider.findOne(item.article_id);
-        console.log('artExist--->', artExist);
-        if (artExist) {
-          continue;
-        }
-        await spider.saveArticle(item);
-        this.num++;
-        console.log('saveArticle', item.title, this.num);
+      for (const item of data) {
+        this.detail(item);
       }
     } catch (err) {
       console.log('spider.hander.error', err.name, err.message);
+    }
+  }
+
+  detail = async (item) => {
+    try {
+      item = util.filter(item, ['title', 'description', 'url', 'display_time', 'thumb', 'id']);
+      const articleText = await SpiderArticle.scratch(item.url);
+      item.text = articleText;
+      const display_time = util.formatTime(item.display_time);
+      item.display_time = display_time && new Date(display_time).toISOString();
+      const artExist = await spider.findOne(item.article_id);
+      console.log('artExist--->', artExist);
+      if (artExist) {
+        return;
+      }
+      await spider.saveArticle(item);
+      this.num++;
+      console.log('saveArticle', item.title, this.num);
+    } catch (err) {
+      console.log('spider.detail.error', err.name, err.message);
     }
   }
 };
--- src/util/handler_event.js
+++ src/util/handler_event.js
@@ -1,9 +1,13 @@
 import EventEmitter from 'events';
 
 class HandlerEvent extends EventEmitter {
-  enQueue = async (pro) => {
+  enQueue = (pro) => {
     this.emit('handle', pro);
   }
+
+  // articleDetail = (item) => {
+  //   this.emit('article', item);
+  // }
 }
 
 export default HandlerEvent;
